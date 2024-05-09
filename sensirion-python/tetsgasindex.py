2e87013b7e5cf3165f22f37bfb79caaafeba4fb0
Nikolay Kochev <nick@uni-plovdiv.net>
2024-05-09 15:47:41 +0300

    Imroved test functions: iterateCsvFile() and lineAction(). Added class TaskType

--- sensirion-python/tetsgasindex.py
+++ sensirion-python/tetsgasindex.py
@@ -3,8 +3,12 @@ import csv
 from calcgasindex import *
 
 
+class TaskType:
+    PRINT = 0
+    CALC_GAS_INDEX = 1
 
-def iterateCsvFile(fileName: str, startLineNumber = 1, maxLineNumber = 1000000000):
+
+def iterateCsvFile(fileName: str, taskType: int, startLineNumber = 1, maxLineNumber = 1000000000):
     with open(fileName, mode ='r') as file:
         csvFile = csv.reader(file)
         n = 0
@@ -14,11 +18,18 @@ def iterateCsvFile(fileName: str, startLineNumber = 1, maxLineNumber = 100000000
                 continue            
             if (n > maxLineNumber):
                 break
-            print(line)    
-            lineAction(n, line)
+            lineAction(n, line, taskType)
 
-def lineAction(n:int, line: []):
-    pass
+
+def lineAction(n:int, line: [], taskType :int):
+    match taskType:
+        case TaskType.PRINT:
+            print(line)
+            
+        case TaskType.CALC_GAS_INDEX:
+            #TODO
+            pass
+         
 
 
 
