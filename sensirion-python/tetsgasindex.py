cb20e36cf58e112977bd6d8474356ca6fd3365b8
Nikolay Kochev <nick@uni-plovdiv.net>
2024-05-09 12:10:02 +0300

    Added function iterateCsvFile()

--- sensirion-python/tetsgasindex.py
+++ sensirion-python/tetsgasindex.py
@@ -1,12 +1,25 @@
 
+import csv
 from calcgasindex import *
 
-# Some Testing
-params =  GasIndexAlgorithmParams()
-#GasIndexAlgorithm_reset(params)
-GasIndexAlgorithm_init_with_sampling_interval(params, 0, 300)
-#GasIndexAlgorithm_init(params, 0)
 
-print(params.__dict__)
+
+def iterateCsvFile(fileName: str, startLineNumber = 1, maxLineNumber = 1000000000):
+    with open(fileName, mode ='r') as file:
+        csvFile = csv.reader(file)
+        n = 0
+        for line in csvFile:
+            n = n+1
+            if (n < startLineNumber):
+                continue            
+            if (n > maxLineNumber):
+                break
+            print(line)    
+            lineAction(n, line)
+
+def lineAction(n:int, line: []):
+    pass
+
+
 
 
