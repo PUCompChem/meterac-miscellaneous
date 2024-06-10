b523210f1cdaf51d1890951d39ff95ea901cc30e
Nikolay Kochev <nick@uni-plovdiv.net>
2024-06-10 22:58:28 +0300

    Added API startup and shutdown handlers

--- sensirion-python/fastapiservice.py
+++ sensirion-python/fastapiservice.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def startup():
+    print("API startup ---- ")
+
+def shutdown():
+    print("API shutdown ---- ")
+
+app.add_event_handler('startup', startup)
+app.add_event_handler('shutdown', shutdown)
+
+
 @app.get("/")
 def root():
     return {"Info": "Sensirion VOC/NOX calculation utils"}
