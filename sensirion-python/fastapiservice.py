5773aaf3b2cd34ce5ce43762e6a0569a7e005640
Nikolay Kochev <nick@uni-plovdiv.net>
2024-06-12 00:12:50 +0300

    Added new class Node and function debugInit(). Added fastapi route 'nodecount'

--- sensirion-python/fastapiservice.py
+++ sensirion-python/fastapiservice.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
+
+class Node(BaseModel):
+    name: str
+    class Config:
+        arbitrary_types_allowed = True
+
+nodes = []
+
+
+def debugInit():
+    print("Debug Init: adding two demo nodes")
+    nodes.append(Node(name = "N00T1"))
+    nodes.append(Node(name = "N00T2"))
 
 def startup():
     print("API startup ---- ")
+    debugInit()
 
 def shutdown():
     print("API shutdown ---- ")
 
+app = FastAPI()
 app.add_event_handler('startup', startup)
 app.add_event_handler('shutdown', shutdown)
 
@@ -15,3 +30,9 @@ app.add_event_handler('shutdown', shutdown)
 @app.get("/")
 def root():
     return {"Info": "Sensirion VOC/NOX calculation utils"}
+
+@app.get("/nodecount")
+def nodecount():
+    count = len(nodes)
+    return {"NodeCount": count}
+    #return count
