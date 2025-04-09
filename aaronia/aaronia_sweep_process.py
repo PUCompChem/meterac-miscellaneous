bbf9794ede6d111752df87cfbc54e8a17031c6bc
Nikolay Kochev <nick@uni-plovdiv.net>
2025-04-09 17:33:44 +0300

    added function float_values_from_string()

--- aaronia/aaronia_sweep_process.py
+++ aaronia/aaronia_sweep_process.py
@@ -1,8 +1,23 @@
 
 
+
+def float_values_from_string(s : str, splitter : str = ";" ) -> list[float]:
+    tokens = s.split(splitter)
+    values = []
+    for tok in tokens:
+        t = tok.strip()
+        if t != '':
+            values.append(float(tok))
+    return values
+
+
 def extract_matrix_from_file(fileName: str):
+    prev_line = None
     with open(fileName, mode ='r') as file:
         n = 0
         for line in file:
             n = n+1
-            print(n,line)
+            #print(n,line)
+            if (not line.startswith("#")):
+                values = float_values_from_string(line)
+                print(values)
