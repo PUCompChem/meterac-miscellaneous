0bedb31ba52161fb691d5732d23d2497e4d43718
Nikolay Kochev <nick@uni-plovdiv.net>
2025-04-09 18:19:15 +0300

    Implemented basic utils for extracing aaronia data as matrix. Function refactoring: extract_matrix_from_file() --> extract_matrix_from_aaronia_file()

--- aaronia/aaronia_sweep_process.py
+++ aaronia/aaronia_sweep_process.py
@@ -1,4 +1,5 @@
 
+frequencies = None
 
 
 def float_values_from_string(s : str, splitter : str = ";" ) -> list[float]:
@@ -11,13 +12,38 @@ def float_values_from_string(s : str, splitter : str = ";" ) -> list[float]:
     return values
 
 
-def extract_matrix_from_file(fileName: str):
-    prev_line = None
+def extract_matrix_from_aaronia_file(fileName: str) -> list:
+    flag_frequencies = False
+    sweepStart = None
+    sweepStop = None
+    matrix = []
     with open(fileName, mode ='r') as file:
         n = 0
         for line in file:
             n = n+1
+            #scip empty line
+            if (line.strip() == ''):                
+                continue
+
             #print(n,line)
-            if (not line.startswith("#")):
+            if (line.startswith("#")):
+                if (line.startswith("# SweepFrequencies=")):
+                   flag_frequencies = True
+                   continue 
+                if (line.startswith("# SweepStart=")):
+                    sweepStart = line.strip()[13:]
+                    continue
+                if (line.startswith("# SweepStop=")):
+                    sweepStop = line.strip()[12:]
+                    continue
+
+            else:
                 values = float_values_from_string(line)
-                print(values)
+                #print(values)
+                matrix.append([sweepStart, sweepStop] + values)
+                #Reset work variables
+                flag_frequencies = False
+                sweepStart = None
+                sweepStop = None           
+
+    return matrix
